4da506a118d0e60456c9145884321f16b161dcdf
ArcaneSunku <t4hnner@gmail.com>
2022-04-18 12:00:50 -0500

    Fixed the readFileToString method, so we close everything we need to.

--- engine/java/git/crystal/engine/utils/Files.java
+++ engine/java/git/crystal/engine/utils/Files.java
@@ -1,11 +1,10 @@
 package git.crystal.engine.utils;
 
 import java.io.*;
-import java.util.Scanner;
 
 /**
  * Handles everything that has to do with File related operations within our Game Engine.
- * Such as reading from or writing to a file.
+ * Such as basic reading or writing operations on a file.
  *
  * @author Tahnner Shambaugh (ArcaneSunku)
  * @date 4/17/2022
@@ -13,41 +12,58 @@ import java.util.Scanner;
 
 public class Files {
 
+    /**
+     * Reads any text file from within our jar and turns it into a String that we can use in our program.
+     *
+     * @param filePath the path of the file we want to read into a String
+     * @return the contents of the file, converted into a String
+     */
     public static String readFileToString(String filePath) {
         return readFileToString(filePath, true);
     }
 
+    /**
+     * Reads any text file and turns it into a String we can use in our program.
+     *
+     * @param filePath the path of the file we want to read into a String
+     * @param internal whether you want to read this from within the jar file or not
+     * @return the contents of the file, converted into a String
+     */
     public static String readFileToString(String filePath, boolean internal) {
         String result;
+        BufferedReader br;
+        StringBuilder sb;
         try {
             if(internal) {
                 InputStream is = Files.class.getResourceAsStream(filePath);
                 if(is == null)
                     throw new IOException(String.format("Failed to load file: [%s]", filePath));
 
-                BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                StringBuilder sb = new StringBuilder();
+                br = new BufferedReader(new InputStreamReader(is));
+                sb = new StringBuilder();
 
                 String line = br.readLine();
                 do {
                     sb.append(line).append("\n");
                 } while((line = br.readLine()) != null);
 
-                br.close();
                 is.close();
-                result = sb.toString();
             } else {
                 File file = new File(filePath);
-                BufferedReader br = new BufferedReader(new FileReader(file));
-                StringBuilder sb = new StringBuilder();
+                if(!file.exists())
+                    throw new IOException(String.format("Failed to load file: [%s]", filePath));
+
+                br = new BufferedReader(new FileReader(file));
+                sb = new StringBuilder();
 
                 String line = br.readLine();
                 do {
                     sb.append(line).append("\n");
                 } while((line = br.readLine()) != null);
-
-                result = sb.toString();
             }
+
+            br.close();
+            result = sb.toString();
         } catch (IOException e) {
             e.printStackTrace(System.err);
             return "nil";
